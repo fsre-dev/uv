d2b85c033e4a67d00c5e8be7b6f70f1c51fa1f88
unknown <fsre-dev@outlook.com>
2019-05-02 15:27:21 +0200

    Postavljanje projekta

---
+++ src/main/java/com/jis/uv/UvApplication.java
@@ -0,0 +1,13 @@
+package com.jis.uv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jis/uv/UvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jis.uv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
